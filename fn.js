769c1c5cc96a543ead9d87fd1a2c478d04aeb3ca
Mike Dominice <mike@mikedominice.com>
2012-07-19 10:41:00 -0400

    wrapped matcher/transforms definition in local module

--- fn.js
+++ fn.js
@@ -6,144 +6,156 @@
     define('fn', function (require, exports) {
         
         var hasOwnProp = Object.prototype.hasOwnProperty,
-        fn = {},
-        transforms = {},
-        matchers = {};
+        fn = {};
         
         // transforms are functions that return functions that transform a variable.
         // useful for passing to things like map().
-        transforms.property = function (propertyName) {
-            return function (object) {
-                return object[propertyName];
+        fn.transforms = (function () {
+            
+            var transforms = {};
+            transforms.property = function (propertyName) {
+                return function (object) {
+                    return object[propertyName];
+                };
             };
-        };
-        
-        transforms.modelAttribute = function (attributeName) {
-            return function (object) {
-                return object.get(attributeName);
+            
+            transforms.modelAttribute = function (attributeName) {
+                return function (object) {
+                    return object.get(attributeName);
+                };
             };
-        };
-        
-        transforms.not = function () {
-            return function (object) {
-                return !object;
+            
+            transforms.not = function () {
+                return function (object) {
+                    return !object;
+                };
             };
-        };
-        
-        transforms.negate = function () {
-            return function (object) {
-                return -object;
+            
+            transforms.negate = function () {
+                return function (object) {
+                    return -object;
+                };
             };
-        };
-        
-        transforms.toInteger = function () {
-            return function (object) {
-                return parseInt(object, 10);
+            
+            transforms.toInteger = function () {
+                return function (object) {
+                    return parseInt(object, 10);
+                };
             };
-        };
-        
-        transforms.replace = function (regex, replacement) {
-            return function (string) {
-                return string.replace(regex, replacement);
+            
+            transforms.replace = function (regex, replacement) {
+                return function (string) {
+                    return string.replace(regex, replacement);
+                };
             };
-        };
+            
+            return transforms;
+            
+        }).call(this);
         
         // matchers are functions return functions that compare an object to a set of conditions.
         // userful for passing to bulk filtering functions.
         // should support partial application.
-        matchers.eq = function (value, other) {
-            if (arguments.length > 1) {
-                return value === other;
-            }
-            return function (object) {
-                return matchers.eq(object, value);
-            };
-        };
-        
-        matchers.gt = function (value, other) {
-            if (arguments.length > 1) {
-                return value > other;
-            }
-            return function (object) {
-                return matchers.gt(object, value);
-            };
-        };
-        
-        matchers.lt = function (value, other) {
-            if (arguments.length > 1) {
-                return value < other;
-            }
-            return function (object) {
-                return matchers.lt(object, value);
-            };
-        };
-        
-        matchers.neq = function (value, other) {
-            if (arguments.length > 1) {
-                return value !== other;
-            }
-            return function (object) {
-                return matchers.neq(object, value);
-            };
-        };
-        
-        matchers['undefined'] = function (other) {
-            if (arguments.length > 0) {
-                return other === void 0;
-            }
-            return function (object) {
-                return matchers['undefined'](object);
-            };
-        };
-        
-        matchers.defined = function (other) {
-            if (arguments.length > 0) {
-                return other !== void 0;
-            }
-            return function (object) {
-                return matchers.defined(object);
-            };
-        };
-        
-        // do the mapping
-        (function () {
+        fn.matchers = (function () {
             
-            function attachMatcher(result, matcherName, matcher) {
-                result[matcherName] = function () {
-                    var createdMatcher = matcher.apply(this, arguments);
-                    return function (object) {
-                        return createdMatcher(result(object));
-                    };
-                };
-            }
+            var matchers = {};
             
-            function attachTransform(transformName, transform) {
-                fn[transformName] = function () {
-                    var result = transform.apply(this, arguments);
-                    for (var matcherName in matchers) {
-                        if (hasOwnProp.call(matchers, matcherName)) {
-                            attachMatcher(result, matcherName, matchers[matcherName]);
-                        }
-                    }
-                    return result;
-                };
-            }
-            
-            for (var transformName in transforms) {
-                if (hasOwnProp.call(transforms, transformName)) {
-                    attachTransform(transformName, transforms[transformName]);
+            matchers.eq = function (value, other) {
+                if (arguments.length > 1) {
+                    return value === other;
                 }
-            }
+                return function (object) {
+                    return matchers.eq(object, value);
+                };
+            };
+            
+            matchers.gt = function (value, other) {
+                if (arguments.length > 1) {
+                    return value > other;
+                }
+                return function (object) {
+                    return matchers.gt(object, value);
+                };
+            };
+            
+            matchers.lt = function (value, other) {
+                if (arguments.length > 1) {
+                    return value < other;
+                }
+                return function (object) {
+                    return matchers.lt(object, value);
+                };
+            };
+            
+            matchers.neq = function (value, other) {
+                if (arguments.length > 1) {
+                    return value !== other;
+                }
+                return function (object) {
+                    return matchers.neq(object, value);
+                };
+            };
+            
+            matchers['undefined'] = function (other) {
+                if (arguments.length > 0) {
+                    return other === void 0;
+                }
+                return function (object) {
+                    return matchers['undefined'](object);
+                };
+            };
+            
+            matchers.defined = function (other) {
+                if (arguments.length > 0) {
+                    return other !== void 0;
+                }
+                return function (object) {
+                    return matchers.defined(object);
+                };
+            };
+            
+            // do the mapping
+            (function () {
+                
+                function attachMatcher(result, matcherName, matcher) {
+                    result[matcherName] = function () {
+                        var createdMatcher = matcher.apply(this, arguments);
+                        return function (object) {
+                            return createdMatcher(result(object));
+                        };
+                    };
+                }
+                
+                function attachTransform(transformName, transform) {
+                    fn[transformName] = function () {
+                        var result = transform.apply(this, arguments);
+                        for (var matcherName in matchers) {
+                            if (hasOwnProp.call(matchers, matcherName)) {
+                                attachMatcher(result, matcherName, matchers[matcherName]);
+                            }
+                        }
+                        return result;
+                    };
+                }
+                
+                for (var transformName in fn.transforms) {
+                    if (hasOwnProp.call(fn.transforms, transformName)) {
+                        attachTransform(transformName, fn.transforms[transformName]);
+                    }
+                }
+                
+            }).call(this);
+            
+            return matchers;
+            
         }).call(this);
         
         // extend fn with matchers
-        for (var key in matchers) {
-            if (hasOwnProp.call(matchers, key)) {
-                fn[key] = matchers[key];
+        for (var key in fn.matchers) {
+            if (hasOwnProp.call(fn.matchers, key)) {
+                fn[key] = fn.matchers[key];
             }
         }
-        
-        fn.transforms = transforms;
         
         //Attach properties to exports.
         module.exports = fn;
